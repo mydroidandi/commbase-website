d30750c4f708e64ea9a6cf2048d94ca09c7652b5
Esteban Herrera <stv.herrera@gmail.com>
2022-11-17 22:30:51 -0600

    Changelog links

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,11 +3,18 @@ export default {
     logo: "/commbase.png",
     siteTitle: "Commbase",
     nav: [
-    { text: "About", link: "/about" },
-    { text: "Contact", link: "/contact" },
-    { text: "Guide", link: "/guide" },
-    { text: "Install", link: "/configs" },
-    { text: "Changelog", link: "https://github.com/mydroidandi/commbase" },
+      { text: "About", link: "/about" },
+      { text: "Contact", link: "/contact" },
+      { text: "Guide", link: "/guide" },
+      { text: "Install", link: "/configs" },
+      { text: "Changelog",
+          items: [
+            { text: "v0.0.1", link: "/item-1" },
+            { text: "v0.0.2", link: "/item-2" },
+            { text: "v0.0.3", link: "/item-3" },
+          ],
+      },
     ],
   },
 };
+
